6b02f84f0880248e288291baa0d3f4d36a71a509
Elena Troeglazova <BadIllusion@yandex.ru>
2026-08-02 00:19:45 +0300

    Исправить ошибки сборки

--- src/ru/yandex/practicum/Wordle.java
+++ src/ru/yandex/practicum/Wordle.java
@@ -2,7 +2,6 @@ package ru.yandex.practicum;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
